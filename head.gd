fcb5bf98aee0e0a25fdca1c894ef35fe78d96031
Scott Logan <thecaveisstilldark@gmail.com>
2024-09-04 13:19:08 -0400

    Adjusted grabbing system

--- head.gd
+++ head.gd
@@ -3,21 +3,31 @@ extends RigidBody2D
 var hover = false
 var grabbed = false
 
+func _ready() -> void:
+	pass
 
 func _process(delta: float) -> void:
+	#print("Hover: ", hover)
 	if Input.is_action_pressed("grab"):
-		#if hover:
-		if grabbed:
-			
-		else:
+		print("Trying to Grab")
+		print("Hover: ", hover)
+		if hover:
+			print("Grabbed!")
 			grabbed = true
 	else:
 		grabbed = false
+		
 	
+	if grabbed:
+		get_parent().get_node("ScreenInfo/TargetPin").node_a = get_path()
+	else:
+		get_parent().get_node("ScreenInfo/TargetPin").node_a = get_parent().get_node("ScreenInfo/Dummy").get_path()
 
 
 func _on_mouse_entered() -> void:
 	hover = true
+	print("hover")
 
 func _on_mouse_exited() -> void:
 	hover = false
+	print("stop hover")
--- shrimpcat_modular.gd
+++ shrimpcat_modular.gd
@@ -8,11 +8,18 @@ func _ready() -> void:
 	var prev_segment = $Head
 	for i in range(num_segments):
 		var new_segment = segment.instantiate()
-		new_segment.position = Vector2(0, (i+1) * 70)
+		new_segment.position = Vector2(0, (i+1) * 80)
 		add_child(new_segment)
 		new_segment.get_node("Pin_Prev").node_b = prev_segment.get_path()
 		prev_segment = new_segment
-		
-	$Butt.position = Vector2(0, (num_segments+1) * 70)
+		get_parent().get_node("Border").set_collision_layer_value(i+2, true)
+		get_parent().get_node("Border").set_collision_mask_value(i+2, true)
+		new_segment.set_collision_layer_value(i+2, true)
+		new_segment.set_collision_mask_value(i+2, true)
+	$Butt.position = Vector2(0, (num_segments+1) * 80)
 	$Butt/ButtPin.node_b = prev_segment.get_path()
 	move_child($Head, get_child_count())
+
+
+func _process(delta: float) -> void:
+	print(transform)
